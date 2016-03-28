8c4b0e1c708cac26f44e3907bc52ca6872e2580d
Eduardo Antonio Niehues <eduardo@edanni.io>
2016-03-28 13:23:39 -0300

    Route points

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,6 @@ class Route < ApplicationRecord
   belongs_to :line
   has_many :child_routes, class_name: 'Route', foreign_key: :parent_route_id
   has_many :times, class_name: 'Timetable'
+  has_many :route_points, -> { order('"order" ASC')}
+  has_many :points, through: :route_points
 end
