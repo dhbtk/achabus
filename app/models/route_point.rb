51bf66121c39b24771dcb726693e5d8d80ac418f
Eduardo Antonio Niehues <eduardo@edanni.io>
2016-06-18 16:03:09 -0300

    começo do pathfind

--- app/models/route_point.rb
+++ app/models/route_point.rb
@@ -1,4 +1,12 @@
 class RoutePoint < ApplicationRecord
   belongs_to :route
   belongs_to :point
+
+  def neighbors
+    RoutePoint.where('route_id <> ? OR "order" > ?', self.route_id, self.order).joins(:point)
+  end
+
+  def cost_to target
+
+  end
 end
