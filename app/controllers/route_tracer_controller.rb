4a41f3ac478857330711590798b9197a0dcf6521
Eduardo Antonio Niehues <eduardo@edanni.io>
2016-06-28 14:03:13 -0300

    Traçador de rotas não precisa de autenticação

--- app/controllers/route_tracer_controller.rb
+++ app/controllers/route_tracer_controller.rb
@@ -1,4 +1,5 @@
 class RouteTracerController < ApplicationController
+  skip_before_action :auth_basic_temp
   def trace
     @source = VirtualPoint.new(params[:src_lon], params[:src_lat])
     @destination = VirtualPoint.new(params[:dest_lon], params[:dest_lat])
