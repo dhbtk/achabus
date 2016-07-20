db10ba44810a23acbfe4f6effec9b8e80873e76d
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-07-20 13:26:08 -0300

    Pathfinding a pé corrigidaço

--- app/models/closest_street_segment.rb
+++ app/models/closest_street_segment.rb
@@ -1,5 +1,5 @@
 class ClosestStreetSegment
-  attr_reader :gid, :point, :source, :target, :path_index, :closest_point_distance, :source_distance, :target_distance
+  attr_reader :gid, :point, :source, :target, :path_index, :closest_point_distance, :source_distance, :target_distance, :source_line, :target_line, :closest_point_line
 
   def self.from_point_and_gid(point, gid)
     a = ClosestStreetSegment.new
@@ -26,7 +26,6 @@ FROM routing.ways
 WHERE st_dwithin(the_geom :: GEOGRAPHY, st_geographyfromtext('#{point_text}'), 50)
 ORDER BY ST_distance(the_geom :: GEOGRAPHY, st_geographyfromtext('#{point_text}'))
 LIMIT 1"
-    puts sql
     @gid = ApplicationRecord.connection.execute(sql).values[0][0]
   end
 
@@ -54,7 +53,22 @@ LIMIT 1"
           FROM routing.ways wa
           WHERE wa.gid = w.gid
           OFFSET (foo.dp).path [1] - 1
-        ) f)                                                                                                AS dist_target
+        ) f)                                                                                                AS dist_target,
+(SELECT st_astext(st_makeline(array_agg((a).geom)) :: GEOGRAPHY)
+   FROM (
+          SELECT st_dumppoints(the_geom) AS a
+          FROM routing.ways wa
+          WHERE wa.gid = w.gid
+          LIMIT (foo.dp).path [1]
+        ) f)                                                                                                AS source_line,
+  (SELECT st_astext(st_makeline(array_agg((a).geom)) :: GEOGRAPHY)
+   FROM (
+          SELECT st_dumppoints(the_geom) AS a
+          FROM routing.ways wa
+          WHERE wa.gid = w.gid
+          OFFSET (foo.dp).path [1] - 1
+        ) f)                                                                                                AS target_line,
+  st_astext(st_makeline(st_closestpoint(st_geographyfromtext('#{@point}')::geometry, w.the_geom), st_geographyfromtext('#{@point}')::geometry)::geography) as closest_point_line
 FROM (
        SELECT st_dumppoints(w.the_geom) AS dp
        FROM routing.ways w
@@ -65,12 +79,15 @@ ORDER BY dist_rp
 LIMIT 1
 "
       res = ApplicationRecord.connection.execute(sql).values[0]
-      @source = res[0]
-      @target = res[1]
-      @path_index = res[2]
-      @closest_point_distance = res[3]
-      @source_distance = res[4]
-      @target_distance = res[5]
+      @source = res[0] # source
+      @target = res[1] # target
+      @path_index = res[2] # idx
+      @closest_point_distance = res[3] # dist_cp
+      @source_distance = res[5]
+      @target_distance = res[6]
+      @source_line = res[7]
+      @target_line = res[8]
+      @closest_point_line = res[9]
 
       [res[0], res[1]]
     end
